8ceecf0f6a30974fda3fd50245cf528fe6b586b6
Osas706 <92319443+Osas706@users.noreply.github.com>
2024-05-20 23:45:03 +0100

    login and signup ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./components/navbar/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Cart from "./pages/cart/Cart";
 import Order from "./pages/placeOrder/Order";
 import Footer from "./components/footer/Footer";
+import Login from "./components/login/Login";
 
 const App = () => {
+  const [showLogin, setShowLogin] = useState(false);
+
   return (
     <>
+      {showLogin ? <Login setShowLogin={setShowLogin} /> : ''}
+
       <div className="app">
-        <Navbar />
+        <Navbar  setShowLogin={setShowLogin} />
 
         <Routes>
           <Route path="/" element={<Home />} />
