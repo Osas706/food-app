9462059b855544b627733848cf0d7bd1a584e31f
Osas706 <92319443+Osas706@users.noreply.github.com>
2024-05-27 10:31:30 +0100

    ready for deploayment

--- frontend/src/context/storeContext.jsx
+++ frontend/src/context/storeContext.jsx
@@ -4,7 +4,7 @@ import { createContext, useEffect, useState } from "react";
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => { 
-  const url = 'http://localhost:4000';
+  const url = 'https://food-app-mern-j4kd.onrender.com';
   const [cartItems, setCartItems ] = useState({});
   const [token, setToken] = useState('');
   const [food_list, setFoodList] = useState([]);
