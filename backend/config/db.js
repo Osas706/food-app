19bcbd12d4def07060d248d9fea99f5039179ecd
Osas706 <92319443+Osas706@users.noreply.github.com>
2024-09-10 22:05:49 +0100

    db

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://food-app:food-app@cluster0.wbuguuo.mongodb.net/')
+    await mongoose.connect('mongodb+srv://food-app:food-app@cluster0.wbuguuo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
       .then(() => console.log('Connected to database'))
 };
